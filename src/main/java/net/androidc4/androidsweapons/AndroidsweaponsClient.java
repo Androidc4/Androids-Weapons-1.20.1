b484fb5439b8e2d633bd642642b858ef20571b51
ANDROIDC4\Vasin <vasinacek@seznam.cz>
2024-01-28 20:17:30 +0100

    Initial commit

---
+++ src/main/java/net/androidc4/androidsweapons/AndroidsweaponsClient.java
@@ -0,0 +1,10 @@
+package net.androidc4.androidsweapons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AndroidsweaponsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/androidc4/androidsweapons/AndroidsweaponsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.androidc4.androidsweapons;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AndroidsweaponsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
